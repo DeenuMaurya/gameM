8619a105bcd216a2b93e229de844165dd0c62518
Ashok <cmaurya46@gmail.com>
2021-09-06 12:34:28 +0530

    new

---
+++ Base Project/Assets/Player/MovementController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementController : MonoBehaviour
+{
+    public static MovementController instance;
+    public float speed = 3.0F;
+    private CharacterController controllor;
+    public static bool IsMoving = false;
+    public static float curSpeed;
+    private Transform vrHead;
+
+    Vector3 forward;
+
+    void Start()
+    {
+        instance = this;
+        controllor = GetComponent<CharacterController>();
+        vrHead = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (VrSelector.instance.IsDaydream)
+        {
+            #if UNITY_EDITOR
+            NormalController();
+            #endif
+
+            forward = vrHead.TransformDirection(Vector3.forward);
+            Vector2 TouchAxis = GvrControllerInput.TouchPosCentered;
+
+            if (!GvrController.IsTouching)
+            {
+                curSpeed = 0;
+                return;
+            }
+
+            if (TouchAxis.magnitude > 0.3f)
+            {
+                if (Mathf.Abs(TouchAxis.x) <= Mathf.Abs(TouchAxis.y))
+                {
+                    curSpeed = speed * TouchAxis.y;
+                    controllor.SimpleMove(forward * curSpeed);
+                } 
+            }
+
+
+        }
+        else if (VrSelector.instance.IsOculus)
+        {
+            #if UNITY_EDITOR
+            NormalController();
+            #endif
+
+            forward = vrHead.TransformDirection(Vector3.forward);
+            Vector2 TouchAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+
+            if (!OVRInput.Get(OVRInput.Touch.PrimaryTouchpad))
+            {
+                curSpeed = 0;
+                return;
+            }
+
+            if (TouchAxis.magnitude > 0.3f)
+            {
+                if (Mathf.Abs(TouchAxis.x) <= Mathf.Abs(TouchAxis.y))
+                {
+                    curSpeed = speed * TouchAxis.y;
+                    controllor.SimpleMove(forward * curSpeed);
+                } 
+            }
+        }
+        else if (VrSelector.instance.IsCardboard)
+        {
+            NormalController();
+        }
+
+        if (curSpeed == 0)
+            IsMoving = false;
+        else
+            IsMoving = true;
+    }
+
+    void NormalController()
+    {
+        forward = vrHead.TransformDirection(Vector3.forward);
+        curSpeed = speed * Input.GetAxis("Vertical");
+        controllor.SimpleMove(forward * curSpeed);
+    }
+
+}
---
+++ Base Project/Assets/SoundManager/Scripts/SoundManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    public AudioClip ClickSound;
+    public AudioClip ScrollSound;
+    public AudioClip RightSound;
+    public AudioClip WrongSound;
+    public AudioClip MessageSound;
+    AudioSource asc;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        asc = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void PlayClickSound()
+    {
+        SoundManager.instance.asc.clip = ClickSound;
+        SoundManager.instance.asc.PlayOneShot(ClickSound);
+    }
+
+    public void PlayScrollSound()
+    {
+        SoundManager.instance.asc.clip = ScrollSound;
+        SoundManager.instance.asc.PlayOneShot(ScrollSound);
+    }
+
+    public void PlayRightSound()
+    {
+        SoundManager.instance.asc.clip = RightSound;
+        SoundManager.instance.asc.PlayOneShot(RightSound);
+    }
+
+    public void PlayWrongSound()
+    {
+        SoundManager.instance.asc.clip = WrongSound;
+        SoundManager.instance.asc.PlayOneShot(WrongSound);
+    }
+
+    public void PlayMessageSound()
+    {
+        SoundManager.instance.asc.clip = MessageSound;
+        SoundManager.instance.asc.PlayOneShot(MessageSound);
+    }
+
+    public void PlayAudioClip(AudioClip _clip)
+    {
+        SoundManager.instance.asc.clip = _clip;
+        SoundManager.instance.asc.PlayOneShot(_clip);
+    }
+}
